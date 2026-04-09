bab540ddb17ba83e16e6dc72aa133ddaf1a814a6
maramire2001 <marioantonioramirezbarajas@gmail.com>
2026-04-09 15:06:15 -0600

    Fix: Instalación de TypeScript y limpieza de config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Disabling strict mode for p5.js compatibility
-  swcMinify: true,
 };
 
 export default nextConfig;
